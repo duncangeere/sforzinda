a7c96dad51322972ba759238776ad9284103f127
Duncan Geere <radio.edit@gmail.com>
2020-10-29 14:15:27 +0100

    Commenting

--- sketch.js
+++ sketch.js
@@ -25,10 +25,8 @@ const marketPos = Math.floor(Rune.random(0, gates))
 // Group to hold the city
 const city = r.group(r.width / 2, r.height / 2).rotate(90, r.width / 2, r.height / 2);
 
-// Outer and inner circles, coverted to polygons.
-//const outerC1 = new Rune.Circle(0, 0, outerR + outerDiffRad).toPolygon({ spacing: 10 }, city).fill("none");
-const outerC2 = new Rune.Circle(0, 0, outerR).toPolygon({ spacing: 10 }, city).fill("none") //.stroke("none");
-//const outerC3 = new Rune.Circle(0, 0, outerR - outerDiffRad).toPolygon({ spacing: 10 }, city).fill("none");
+// Outer and inner circles, converted to polygons.
+const outerC2 = new Rune.Circle(0, 0, outerR).toPolygon({ spacing: 10 }, city).fill("none")
 const concaveC = new Rune.Circle(0, 0, concaveR).toPolygon({ spacing: 10 }, city).fill("none").stroke("none");
 const innerC = new Rune.Circle(0, 0, innerR).toPolygon({ spacing: 10 }, city).fill("none");
 
@@ -38,13 +36,12 @@ const perimeter = r.path(0, 0, city).fill("none").moveTo(outerR, 0)
 // Spin around the spokes and draw everything
 for (let i = 0; i < gates; i++) {
 
-    //Grab positions on circles
+    // Grab positions on circles
     let outer2 = outerC2.vectorAt(i / gates);
-    //let outer3 = outerC3.vectorAt(i / gates);
     let concave = concaveC.vectorAt(i / gates);
     let inner = innerC.vectorAt(i / gates);
 
-    // Check if even or odd, then draw the appropriate bits
+    // Check if even or odd, then draw the appropriate bits. If even:
     if (i % 2 == 0) {
 
         // Draw a circle and a rect
@@ -57,8 +54,9 @@ for (let i = 0; i < gates; i++) {
         // Add a node to the perimeter path
         perimeter.lineTo(outer2.x, outer2.y)
 
-    } else {
+    } else { // if odd
 
+        // Put the word "Porta" in, rotated after half way round
         if (i >= gates / 2) {
             r.text("Porta", concaveR + portaOffset, 5, city).rotate(360 * i / gates, 0, 0).fontSize("140%").fill("none")
         } else {
@@ -66,6 +64,7 @@ for (let i = 0; i < gates; i++) {
         	r.text("Porta", -46, 10, text).fontSize("140%").fill("none").rotate(180, 0, 0)
         }
 
+        // Draw a rectangle to mark the gate
         r.rect(innerR - 7.5, -7.5, 15, 15, city).fill("none").rotate(360 * i / gates, 0, 0)
 
         // Draw a line from the centre
@@ -77,6 +76,7 @@ for (let i = 0; i < gates; i++) {
 
     // Draw a church on one of the spokes
     if (i == churchPos) {
+        
         // Group to hold it 
         const church = r.group(innerR - 65, 10, city).rotate(360 * i / gates, 0, 0);
 
